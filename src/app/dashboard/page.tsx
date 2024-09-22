c312a253f7c3b4dd088053f5e208754ddd914a73
mishraprayash <mishraprayash11@gmail.com>
2024-09-22 13:51:21 +0545

    added signout button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
+import SignOutButton from "@/components/SignOutButton";
 import { getUser } from "@/lib/lucia";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const DashboardPage = async () => {
-    // protected!!!
+  // protected!!!
   const user = await getUser();
   if (!user) {
     redirect("/authenticate");
   }
-  return <div>Your are logged in as {user.name} with an email {user.email}</div>;
+  return (
+    <>
+      <div>
+        Your are logged in as {user.name} with an email {user.email}
+      </div>
+      <SignOutButton>Sign Out</SignOutButton>
+    </>
+  );
 };
 export default DashboardPage;
